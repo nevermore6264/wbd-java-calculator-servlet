4e69f64de814663696ebdc19000d219930e469e8
nevermore6264 <nevermore6264@gmail.com>
2018-04-26 16:03:45 +0700

    [Bài tập] Ứng dụng Calculator

---
+++ src/CalculatorServlet.java
@@ -0,0 +1,48 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "CalculatorServlet", urlPatterns = "/calculate")
+public class CalculatorServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        float firstOperand = Integer.parseInt(request.getParameter("first-operand"));
+        float secondOperand = Integer.parseInt(request.getParameter("second-operand"));
+        char operator = request.getParameter("operator").charAt(0);
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<h1>Result:</h1>");
+        try {
+            float result = calculate(firstOperand, secondOperand, operator);
+            writer.println(firstOperand + " " + operator + " " + secondOperand + " = " + result);
+        } catch (Exception ex) {
+            writer.println("Error: " + ex.getMessage());
+        }
+        writer.println("</html>");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    public static float calculate(float firstOperand, float secondOperand, char operator) {
+        switch (operator) {
+            case '+':
+                return firstOperand + secondOperand;
+            case '-':
+                return firstOperand - secondOperand;
+            case '*':
+                return firstOperand * secondOperand;
+            case '/':
+                if (secondOperand != 0)
+                    return firstOperand / secondOperand;
+                else
+                    throw new RuntimeException("Can't divide by zero");
+            default:
+                throw new RuntimeException("Invalid operation");
+        }
+    }
+}
